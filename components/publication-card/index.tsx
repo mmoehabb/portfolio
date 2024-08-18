57c7f7afe257899583c08e1272971d5fe95272fa
Mahmoud E. Abdelsalam <mo.ehab150@gmail.com>
2024-08-18 21:19:17 +0300

    prettier

--- components/publication-card/index.tsx
+++ components/publication-card/index.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import "./index.css"
+
 import Image from "next/image"
 import { Publication } from "@/app/lib/data"
 import { useState, useEffect } from "react"
@@ -13,7 +15,7 @@ export default function PublicationCard({ publication }: { publication: Publicat
   }, [publication.imgsrc])
 
   return (
-    <div className="card bg-neutral h-[550px] w-96 m-8 md:m-12 shadow rounded-lg overflow-hidden transition lg:hover:scale-125 hover:shadow-xl">
+   <div className="card bg-neutral h-[550px] w-96 m-8 md:m-12 shadow rounded-lg overflow-hidden transition lg:hover:scale-125 hover:shadow-xl">
       <a href={publication.link} className="relative">
         <div className={clsx(
           "absolute flex justify-center items-center h-full w-full bg-neutral transition", 
@@ -30,17 +32,18 @@ export default function PublicationCard({ publication }: { publication: Publicat
           onLoad={() => setLoaded(true)}
         />
       </a>
-      <div className="card-body">
-        <h2 className="card-title">
+      <div className="card-details">
+        <h2 className="text-title">
           {publication.title} 
           <div className="badge badge-secondary hidden">NEW</div>
         </h2>
-        <p className="text-justify">{publication.desc}</p>
+        <p className="text-body text-justify">{publication.desc}</p>
         <div className="card-actions justify-end">
           {publication.tags.map((tag, i) => (
             <div key={i} className="badge badge-outline">{tag}</div>
           ))}
         </div>
+        <a href={publication.link} class="card-button">More info</a>
       </div>
     </div>
   )
